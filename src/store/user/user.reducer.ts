11533275cacea8f2652439fdc927bb0f37e9d50c
Enu Azubike <enuazubike88@gmail.com>
2022-08-23 13:11:20 +0100

    cleaned up the user reducer

--- src/store/user/user.reducer.ts
+++ src/store/user/user.reducer.ts
@@ -28,23 +28,23 @@ export const userReducer = (
   if (signInSuccess.match(action.type)) {
     return { ...state, currentUser: action.payload };
   }
-  if (signInFailure.match(action.type)) {
-    return { ...state, error: action.payload };
-  }
+
   if (signupStart.match(action.type)) {
     return { ...state, isLoading: true };
   }
   if (signUpSuccess.match(action.type)) {
     return { ...state, currentUser: action.payload, isLoading: false };
   }
-  if (signupFailure.match(action.type)) {
-    return { ...state, error: action.payload };
-  }
   if (signOutSuccess.match(action.type)) {
     return { ...state, currentUser: null };
   }
-  if (signOutFailure.match(action.type)) {
+  if (
+    signupFailure.match(action.type) ||
+    signInFailure.match(action.type) ||
+    signOutFailure.match(action.type)
+  ) {
     return { ...state, error: action.payload };
   }
+
   return state;
 };
